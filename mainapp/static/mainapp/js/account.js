1083e4d3ddd7e02517077b1315f5246bb9d44e56
nikkimontoya <trentlaneisfrontmanofspiral@gmail.com>
2018-05-28 17:46:30 +0300

    #commit Добавлена поисковая форма на странице "Словарь"

--- mainapp/static/mainapp/js/account.js
+++ mainapp/static/mainapp/js/account.js
@@ -2,4 +2,25 @@
 	$(document).ready(function(){
 		$(".dropdown-trigger").dropdown({ coverTrigger: false });
 	});
+
+	$('#search-words-submit').click(function(e) {
+  		e.preventDefault();
+        var text = $('#search-words-input').val();
+        var csrf_token = $(this).closest('form').find('input[name=csrfmiddlewaretoken]').val();
+
+        if (text != '') {
+        	$.ajax({
+        		url: '/dictionary/',
+        		method: 'POST',
+        		data: {
+        			text: text,
+              		csrfmiddlewaretoken: csrf_token
+        		},
+        		success: function(response) {
+        			$('#result-heading').text(response.Heading);
+              		$('#result-translation').text(response.Translation.Translation);
+        		}
+        	});
+        }
+  	});
 })(jQuery);
