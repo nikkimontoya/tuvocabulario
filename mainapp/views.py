56bf8dee4a2e5f39f19e366118d675d8498c3848
nikkimontoya <trentlaneisfrontmanofspiral@gmail.com>
2018-06-04 21:25:49 +0300

    #commit Добавлены заголовки страниц

--- mainapp/views.py
+++ mainapp/views.py
@@ -16,36 +16,6 @@ def index(request):
 
 token = '';
 def request_to_dictionary(request):
-    '''wordForm = WordForms.objects.filter(form = request.POST['text'])
-    responseObject = {}
-  
-    if wordForm.exists():
-        word = wordForm.first().word
-        responseObject['original'] = word.original
-        responseObject['translation'] = word.translation
-        responseObject['wordId'] = word.id
-    else:
-        global token
-        if token == '':
-            token = requests.post('https://developers.lingvolive.com/api/v1.1/authenticate', headers = {"Authorization": "Basic ZTc1ZDY2MmMtZjkxZi00NTNlLWExY2MtNjA3Y2RlNjJhMDYyOjQzOGZlNzcxNTAxODQzODI4OTA3OWI4MjQ2ZmExZTI1"}, verify = False).text
-    
-        response = requests.get('https://developers.lingvolive.com/api/v1/Minicard', headers = {"Authorization": "Bearer " + token}, params = {"text": request.POST['text'], "srcLang": 1034, "dstLang": 1049}, verify = False)
-        response = json.loads(response.text)
-
-        responseObject['original'] = response['Heading']
-        responseObject['translation'] = response['Translation']['Translation']
-
-        newWord = Dictionary.objects.filter(original = responseObject['original'])
-
-        if newWord.exists():
-            newWord = newWord.first()
-        else:
-            newWord = Dictionary(original = responseObject['original'], translation = responseObject['translation'], soundFileName = response['Translation']['SoundName'])
-            newWord.save()
-
-        responseObject['wordId'] = newWord.id
-        newWord.wordforms_set.create(form = request.POST['text'])'''
-
     '''s = SnowballStemmer('spanish');
     stemed = s.stem(request.POST['text'])'''
     word = UniversalDictionary.objects.filter(original = request.POST['text'])
