f1c81d46c162363748915d9f27d0314afe69c33d
nikkimontoya <trentlaneisfrontmanofspiral@gmail.com>
2018-06-04 20:19:28 +0300

    #commit Удален модуль user

--- user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'user'
-urlpatterns = [
-    path('register/', views.register, name = 'register'),
-    path('auth/', views.auth, name = 'auth'),
-    path('logout/', views.logout_view, name = 'logout')
-]
